0ea3731631ff807ebd9b82889bfb0caa640f6741
adzil <adzil@students.itb.ac.id>
2015-12-06 15:22:40 +0700

    Separate counter from serial buff module

--- src/serial_buff.v
+++ src/serial_buff.v
@@ -1,47 +1,48 @@
 `timescale 1ns/100ps
 
-module serial_buff(din, clk, rst, ena, dout);
+module serial_buff(din, cntin, clk, rst, ena, dout);
     /* Parameter declaration */
+    parameter MOVIN = 1'd0;
     parameter NDATA = 128;
     localparam NDATA_LOG = $clog2(NDATA);
-    
+
     /* Input/output declaration */
     // Input data line
     input din;
+    input [NDATA_LOG-1:0] cntin;
     // Control line
     input clk;
     input rst;
     input ena;
     // Output data line
     output reg [NDATA-1:0] dout;
-    
+
     /* Wire/register declaration */
     wire [NDATA-1:0] buffer;
     //reg [NDATA-1:0] dout;
-    reg [NDATA_LOG-1:0] counter;
-    
+
     /* Module import declaration */
-    shift_reg #(NDATA) shiftRegInst (
+    shift_reg shiftRegInst (
         .din (din),
         .clk (clk),
         .rst (rst),
         .ena (ena),
         .dout (buffer)
     );
-    
+
     always @ (posedge clk or negedge rst) begin
-        if (!rst) begin
+        if (!rst)
             dout <= 'd0;
-            counter <= 'd0;
-        end
         else begin
             if (!ena) begin
-                if (counter == 'd0)
+                if (cntin == 'd0)
                     dout <= buffer;
-                counter <= counter + {{(NDATA_LOG-1){1'd0}},{1'd1}};
-            end
-            else begin
-                counter <= counter;
+                else if (!MOVIN && cntin[1:0] == 'd0) begin
+                    dout[NDATA-1:4] <= dout[NDATA-5:0]
+                    dout[3:0] <= dout[NDATA-1:NDATA-4];
+                end
+                else
+                    dout <= dout;
             end
         end
     end
--- src/serial_buff_tb.v
+++ src/serial_buff_tb.v
@@ -3,25 +3,34 @@
 module serial_buff_tb;
     /* Parameter declaration */
     parameter NDATA = 128;
-    
+
     /* Input/output declaration */
     // Input data line
     reg din;
+    reg cntin;
     // Control line
     reg clk;
     reg rst;
     reg ena;
     // Output data line
     wire [NDATA-1:0] dout;
-    
+
+    counter cnt (
+        .clk (clk),
+        .rst (rst),
+        .ena (ena),
+        .dout (cntin)
+    );
+
     serial_buff dut (
         .din (din),
+        .cntin (cntin),
         .clk (clk),
         .rst (rst),
         .ena (ena),
         .dout (dout)
     );
-    
+
     initial begin
         clk <= 1'd1;
         ena <= 1'd1;
@@ -34,7 +43,7 @@ module serial_buff_tb;
         #5130
         $stop();
     end
-    
+
     integer isloop = 0;
     always begin
         if (!isloop)
@@ -44,7 +53,7 @@ module serial_buff_tb;
             #20
             din <= $random();
     end
-    
+
     // Clock generation
     always begin
         #10
